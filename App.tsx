3b04201312489f02125716a976948351dc4f16c2
EunhaLim <dmsgk1559@gmail.com>
2023-05-19 18:35:17 +0900

    헤더 커스터마이징

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
+import HeaderlessScreen from './screens/HeaderlessScreen';
 
 export type RootStackParamList = {
   Home: undefined;
   Detail: {id: number};
+  Headerless: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -14,13 +16,44 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: '홈'}}
+          options={{
+            title: '홈',
+            // 헤더 블록에 대한 스타일
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            // 헤더의 텍스트, 버튼들 색상
+            headerTintColor: '#fff',
+            // 타이틀 텍스트의 스타일
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
         />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={({route}) => ({
+            title: `상세 페이지 - ${route.params.id}`,
+            headerBackTitle: '',
+          })}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParamList} from '../App';
 
 type DetailScreenProps = StackScreenProps<RootStackParamList, 'Detail'>;
 
 const DetailScreen = ({route, navigation}: DetailScreenProps) => {
-  const {id} = route.params;
-  console.log(route);
+  // const {id} = route.params;
+
+  // useEffect(() => {
+  //   navigation.setOptions({title: `상세 페이지 - ${route.params.id}`});
+  // }, [navigation, route.params.id]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>id: {id}</Text>
-      <View style={styles.buttons}>
-        <Button
-          title="이전"
-          onPress={() => {
-            navigation.pop();
-          }}
-        />
-        <Button
-          title="다음"
-          onPress={() => {
-            // navigation.navigate('Detail', {id: id + 1});
-            navigation.push('Detail', {id: id + 1});
-          }}
-        />
-        <Button title="처음으로" onPress={() => navigation.popToTop()} />
+    <SafeAreaView style={styles.safeView}>
+      <View style={styles.container}>
+        <Text style={styles.text}>id: {route.params.id}</Text>
+        <View style={styles.buttons}>
+          <Button
+            title="이전"
+            onPress={() => {
+              navigation.pop();
+            }}
+          />
+          <Button
+            title="다음"
+            onPress={() => {
+              // navigation.navigate('Detail', {id: route.params.id + 1});
+              navigation.push('Detail', {id: route.params.id + 1});
+            }}
+          />
+          <Button title="처음으로" onPress={() => navigation.popToTop()} />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
+  safeView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
