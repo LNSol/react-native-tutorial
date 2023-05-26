f725cba1e7cd0d043243434ce1b61cb3a135e8fa
EunhaLim <dmsgk1559@gmail.com>
2023-05-26 16:42:08 +0900

    material bottom tab 기본 사용법

--- App.tsx
+++ App.tsx
@@ -1,6 +1,47 @@
+import React from 'react';
+import {
+  NavigationContainer,
+  Route,
+  getFocusedRouteNameFromRoute,
+} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import MainScreen from './screens/MainScreen';
+import ChatScreen from './screens/ChatScreen';
 
+type TScreen = 'Home' | 'Apply' | 'Cart' | 'My';
 
-const App = () => (
+export type StackScreenParamList = {
+  Main: undefined;
+  Chat: undefined;
+};
 
-);
+const Stack = createStackNavigator<StackScreenParamList>();
+
+const App = () => {
+  const getHeaderTitle = (route: Partial<Route<string>>) => {
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+    const routeMap = {
+      Home: '홈',
+      Apply: '신청하기',
+      Cart: '장바구니',
+      My: '내 정보',
+    };
+    return routeMap[routeName as TScreen];
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={({route}) => ({
+            title: getHeaderTitle(route),
+          })}
+        />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 export default App;
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,33 +1,101 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
+import {StyleSheet, Button, Text, View} from 'react-native';
+import {CompositeScreenProps} from '@react-navigation/native';
+import {
+  createMaterialBottomTabNavigator,
+  MaterialBottomTabScreenProps,
+} from '@react-navigation/material-bottom-tabs';
+import {StackScreenProps} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {StackScreenParamList} from '../App';
 
-type IconProps = {
+type TIconProps = {
   color: string;
 };
 
-const HomeScreen = () => (
+type TabScreenParamList = {
+  Home: undefined;
+  Apply: undefined;
+  Cart: undefined;
+  My: undefined;
+};
+
+type HomeScreenProps = CompositeScreenProps<
+  MaterialBottomTabScreenProps<TabScreenParamList, 'Home'>,
+  StackScreenProps<StackScreenParamList>
+>;
+
+const Tab = createMaterialBottomTabNavigator<TabScreenParamList>();
+
+const HomeScreen = ({navigation}: HomeScreenProps) => (
   <View>
     <Text>Home Screen</Text>
+    <Button title="App 채팅 이동하기" onPress={() => navigation.push('Chat')} />
   </View>
 );
 const ApplyScreen = () => <Text>Apply Screen</Text>;
 const CartScreen = () => <Text>Cart Screen</Text>;
 const MyScreen = () => <Text>My Screen</Text>;
 
-const HomeIcon = ({color}: IconProps) => (
+const HomeIcon = ({color}: TIconProps) => (
   <Icon name="home" color={color} size={24} />
 );
-const ApplyIcon = ({color}: IconProps) => (
+const ApplyIcon = ({color}: TIconProps) => (
   <Icon name="add" color={color} size={24} />
 );
-const CartIcon = ({color}: IconProps) => (
+const CartIcon = ({color}: TIconProps) => (
   <Icon name="shopping-cart" color={color} size={24} />
 );
-const MyIcon = ({color}: IconProps) => (
+const MyIcon = ({color}: TIconProps) => (
   <Icon name="person" color={color} size={24} />
 );
 
-const MainScreen = () => <Text>Hi</Text>;
+const MainScreen = () => (
+  <Tab.Navigator
+    initialRouteName="Home"
+    shifting={true}
+    labeled={false}
+    activeColor="#46c4ea"
+    inactiveColor="#ddd"
+    barStyle={styles.tabBar}>
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarIcon: HomeIcon,
+        tabBarBadge: 'new',
+      }}
+    />
+    <Tab.Screen
+      name="Apply"
+      component={ApplyScreen}
+      options={{
+        tabBarIcon: ApplyIcon,
+      }}
+    />
+    <Tab.Screen
+      name="Cart"
+      component={CartScreen}
+      options={{
+        tabBarIcon: CartIcon,
+        tabBarBadge: 2,
+      }}
+    />
+    <Tab.Screen
+      name="My"
+      component={MyScreen}
+      options={{
+        tabBarIcon: MyIcon,
+        tabBarBadge: true,
+      }}
+    />
+  </Tab.Navigator>
+);
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#000',
+  },
+});
+
 export default MainScreen;
